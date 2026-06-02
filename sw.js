c6a98e2a98d949f14b1d100cdbb014ee4ac6ed66
suheeim <suhee.im@gmail.com>
2026-06-02 17:32:52 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'doubles-game-20260602';
+var urlsToCache = [
+  '/doubles-game/',
+  '/doubles-game/index.html',
+  '/doubles-game/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
